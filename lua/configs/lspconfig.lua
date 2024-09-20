1a148c734a7f79017a1692ec324ac6509c86c856
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-09-20 22:09:12 +0700

    Update config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -8,6 +8,7 @@ local servers = {
   "html",
   "cssls",
   "clangd",
+  "tailwindcss",
   "ts_ls",
   "gopls",
   "rust_analyzer",
@@ -50,4 +51,71 @@ lspconfig.pyright.setup {
       },
     },
   },
+}
+
+require("lspconfig").ts_ls.setup {
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = "/Users/haekal/node_modules/@vue/typescript-plugin",
+        languages = { "javascript", "typescript", "vue" },
+      },
+    },
+  },
+  filetypes = {
+    "javascript",
+    "typescript",
+    "vue",
+  },
+}
+
+-- You must make sure volar is setup
+-- e.g. require'lspconfig'.volar.setup{}
+-- See volar's section for more information
+
+local util = require "lspconfig.util"
+local function get_typescript_server_path(root_dir)
+  local global_ts = "/Users/haekal/node_modules/typescript/lib"
+  local found_ts = ""
+  local function check_dir(path)
+    found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+    if util.path.exists(found_ts) then
+      return path
+    end
+  end
+  if util.search_ancestors(root_dir, check_dir) then
+    return found_ts
+  else
+    return global_ts
+  end
+end
+
+lspconfig.volar.setup {
+  on_new_config = function(new_config, new_root_dir)
+    new_config.init_options.typescript.tsdk =
+      get_typescript_server_path(new_root_dir)
+  end,
+
+  filetypes = {
+    "typescript",
+    "javascript",
+    "javascriptreact",
+    "typescriptreact",
+    "vue",
+    "json",
+  },
+}
+
+lspconfig.tailwindcss.setup {
+  root_dir = util.root_pattern(
+    "tailwind.config.js",
+    "tailwind.config.cjs",
+    "tailwind.config.mjs",
+    "tailwind.config.ts",
+    "postcss.config.js",
+    "postcss.config.cjs",
+    "postcss.config.mjs",
+    "postcss.config.ts"
+  ),
 }
