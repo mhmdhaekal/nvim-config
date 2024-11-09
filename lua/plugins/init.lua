c9602149625f179bb2bf90c862417625db330f72
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-11-09 13:39:23 +0700

    add lazygit nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,6 +39,23 @@ return {
     },
   },
   {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+  {
     "folke/trouble.nvim",
     opts = {},
     cmd = "Trouble",
